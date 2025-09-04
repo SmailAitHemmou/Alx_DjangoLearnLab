f29986ab776df7c62780f3ae14123d1257e7b129
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-04 16:40:35 +0100

    UPdate_9

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,8 +9,7 @@ from .models import Book, Library
 
 # --- Function-based view: list all books ---
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-
+    books = Book.objects.all()
     # OPTION A: return plain text (no template)
     # lines = [f"{b.title} by {b.author.name}" for b in books]
     # return HttpResponse("\n".join(lines), content_type="text/plain")
