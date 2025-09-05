24f9573734c1bc1becc3038c8b052acc58893147
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-05 17:44:38 +0100

    UPdate_12

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 # relationship_app/urls.py
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView, register 
+from django.contrib.auth.views import LoginView, LogoutView 
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),  # Your registration function view
+    path('register/', views.register, name='register'),  # Your registration function view
 ]
