decd024dc1697c861e96af99561afab713f4fef3
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-05 17:28:30 +0100

    UPdate_11

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from .views import list_books
 from . import views
+from .views import login_view, logout_view, register_view
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,6 +7,10 @@ from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+
 
 # --- Function-based view: list all books ---
 def list_books(request):
@@ -27,4 +31,33 @@ class LibraryDetailView(DetailView):
     # Prefetch related books + authors for performance
     def get_queryset(self):
         return super().get_queryset().prefetch_related("books__author")
+    
+# LOGIN VIEW
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('book-list')  # redirect after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
 
+# LOGOUT VIEW
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+# REGISTRATION VIEW
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book-list')  # redirect after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
