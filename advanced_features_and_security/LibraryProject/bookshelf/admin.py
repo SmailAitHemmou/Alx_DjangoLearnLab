5f2f3d2850bfefd967cf4fc9bd3a00dd251fdde5
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-11 19:15:23 +0100

    New_UPdate4

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from LibraryProject.accounts.admin import CustomUserAdmin
-from .models import CustomUser
 # Register your models here.
 
 from .models import Book
@@ -8,13 +6,12 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     # columns shown in the list page
-    list_display = ("title", "author", "publication_year")
+    list_display = ("title", "author", "published_date")
     # right-side filters
-    list_filter = ("author", "publication_year")
+    list_filter = ("author", "published_date")
     # top search box
     search_fields = ("title", "author")
     # optional niceties
     ordering = ("title",)
     list_per_page = 25
 
-admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,21 +3,22 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
-
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    def __str__(self):
-        return self.username
     
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
