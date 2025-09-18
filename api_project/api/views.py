d2c17e9da593a52f6632cc98de92d5772563d899
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-18 10:59:46 +0100

    new_app-api-2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
@@ -9,5 +10,10 @@ class BookList(generics.ListAPIView):
     """
     GET /api/books/  -> returns list of all books
     """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# ViewSet for CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
