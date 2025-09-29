9636c021ce2425b5b9e0751e11cdcf37dd4e67c4
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-29 23:32:41 +0100

    UP3-Add_Tags

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
     path("search/", views.post_search, name="post_search"),
+    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
 
 ]
 
