886f03b0faac8dcd1d8463cda1ae5ae886e13e42
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-29 20:45:33 +0100

    update_project

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -22,6 +24,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', kwargs={'pk': self.pk})
     
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, UserUpdateForm, ProfileForm
+from .forms import CustomUserCreationForm, UserUpdateForm, ProfileForm, PostForm
 from .models import Post
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def post_list(request):
@@ -55,3 +58,40 @@ def profile(request):
         'p_form': p_form
     }
     return render(request, 'blog/profile.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html' 
+    context_object_name = 'posts'
+    paginate_by = 5 
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
