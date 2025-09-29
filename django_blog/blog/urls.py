1a5e19527edffa3609524e30f1777b25b06f0b19
SmailAitHemmou <aithemmousmail@gmail.com>
2025-09-29 22:03:42 +0100

    UP2-Add_Comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,9 +18,9 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('post/<int:post_id>/comment/<int:comment_id>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('post/<int:post_id>/comment/<int:comment_id>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 
 ]
 
