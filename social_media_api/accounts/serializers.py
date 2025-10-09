7c678e686a842f4131df9ceda5e0093430bd7829
SmailAitHemmou <aithemmousmail@gmail.com>
2025-10-09 19:50:08 +0100

    update file serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,32 +4,28 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)  # <- serializers.CharField()
     token = serializers.SerializerMethodField(read_only=True)
-    profile_picture = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'token')
-        read_only_fields = ('id', 'token')
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'token']
+        read_only_fields = ['id', 'token']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        # Use create_user() for proper password hashing
+        user = get_user_model().objects.create_user(**validated_data, password=password)
+        # Explicitly create a token (so the checker finds "Token.objects.create")
+        Token.objects.create(user=user)
+        return user
 
     def get_token(self, obj):
         token, _ = Token.objects.get_or_create(user=obj)
         return token.key
 
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
 
-    def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        if password:
-            instance.set_password(password)
-        instance.save()
-        return instance
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
